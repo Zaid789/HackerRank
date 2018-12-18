14a8c461d3ea0159dc9d9a4fb1df8f7286c411df
Zaid789 <45711880+Zaid789@users.noreply.github.com>
2018-12-18 16:53:16 +0530

    Add files via upload

---
+++ A Very Big Sum.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+	int ele;
+	scanf("%d",&ele);
+        long int arr[ele];
+	for(int i = 0 ; i < ele ; i++)
+		scanf("%ld",&arr[i]);
+	long int sum=0;
+	for(int i = 0 ; i < ele ; i++)
+                sum+= arr[i];
+	printf("%ld" , sum);
+}
