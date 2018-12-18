51f81cec9b41743e38796045725a7497a1875f0d
Zaid789 <45711880+Zaid789@users.noreply.github.com>
2018-12-18 17:05:55 +0530

    Update Plus Minus.c

--- Plus Minus.c
+++ Plus Minus.c
@@ -1,3 +1,7 @@
+/*https://www.hackerrank.com/challenges/plus-minus/problem*/
+
+
+
 #include<stdio.h>
 void main()
 {
@@ -6,6 +10,13 @@ void main()
 	int arr[arr_count];
 	for(int i = 0 ; i < arr_count ; i++)
 		scanf("%d",&arr[i]);
+	
+	
+	
+	/*code for Plus Minus*/
+	
+	
+	
 	float p=0,n=0,z=0;
    	for(int i=0;i<arr_count;i++)
    	{
