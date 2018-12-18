0fd1ba66eac9bdeca5d8d18c296057c7f003b886
Zaid789 <45711880+Zaid789@users.noreply.github.com>
2018-12-18 17:09:19 +0530

    Update Staircase.c

--- Staircase.c
+++ Staircase.c
@@ -1,8 +1,11 @@
+/*https://www.hackerrank.com/challenges/staircase/problem*/
+
 #include<stdio.h>
 int main()
 {
 int i,n,j;
 scanf("%d",&n);
+ /*code for Staircase*/
 for(i=1;i<=n;i++)
 {
     for(j=1;j<=n;j++)
