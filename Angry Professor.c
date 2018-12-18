f2b094c6a182d39d0d36cce2a21a68dfbcd4aa7d
Zaid Khan <zaidk789@gmail.com>
2018-12-18 19:37:52 +0530

    Add files via upload

---
+++ Angry Professor.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+char* angryProfessor(int k, int a_count, int* a)
+{
+int  z=0;
+for(int i=0;i<a_count;i++)
+{
+    if(a[i]<=0)
+    z++;
+    
+}
+if(z<k)
+return  "YES";
+else
+return "NO";    
+}
+void main()
+{
+	int test;
+	char result;
+	scanf("%d",&test);
+	while(test)
+	{
+	int z=0;
+	scanf("%d",&z);
+	int k=0;
+	scanf("%d",&k);
+	int a[z];
+	for( int i = 0; i < z ; i++)
+		scanf("%d",&a[i]);
+	char* result = angryProfessor(k, z, a);
+	printf("%s\n",result);
+	test--;
+	}
+}
+
