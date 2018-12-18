f1adbcdbc090db71912112bbb173c4fa9928641f
Zaid789 <45711880+Zaid789@users.noreply.github.com>
2018-12-18 17:03:59 +0530

    Add files via upload

---
+++ Plus Minus.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+void main()
+{
+	int arr_count;
+	scanf("%d" , &arr_count);
+	int arr[arr_count];
+	for(int i = 0 ; i < arr_count ; i++)
+		scanf("%d",&arr[i]);
+	float p=0,n=0,z=0;
+   	for(int i=0;i<arr_count;i++)
+   	{
+       		if(arr[i]==0)
+       		{
+       		z++;
+      	 	}
+       		else if(arr[i]>0)
+       		{
+       		p++;
+       		}
+       		else
+       		{
+       		n++;
+       		}
+   	}
+   printf("%f\n",p/arr_count);
+   printf("%f\n",n/arr_count);
+   printf("%f\n",z/arr_count);
+
+
+}
