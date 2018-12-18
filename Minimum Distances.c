52cd4d43ea61fb0b5a2a90a3d1f03de8bd3e23fc
Zaid Khan <zaidk789@gmail.com>
2018-12-19 00:18:15 +0530

    Add files via upload

---
+++ Minimum Distances.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void main()
+{
+	 long long int a_count;
+	scanf("%llu",&a_count);
+	long long  int a[a_count];
+	for(int r=0;r<a_count;r++)
+		scanf("%llu",&a[r]);
+	int i,j,z=0;
+	for(i=0;i<a_count;i++)
+	{
+    		for(j=i+1;j<a_count;j++)
+    			if(a[i]==a[j])
+    			{
+        			z=j-i;
+    			}
+	}
+	if(z==0)
+	{
+	z=-1;
+	}
+	printf("%d",z);
+}
+
