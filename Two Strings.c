020a03f5ed30c7952133868e025019366c854978
Zaid Khan <zaidk789@gmail.com>
2018-12-18 22:44:14 +0530

    Add files via upload

---
+++ Two Strings.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<string.h>
+char* twoStrings(char* s1, char* s2)
+{
+    long long int i,j,k,z;
+    k=strlen(s1);
+    z=strlen(s2);
+    for(i=0;i<k;i++)
+    {
+        for(j=0;j<z;j++)
+        {
+            if(s1[i]==s2[j])
+            return "YES";
+        }
+    }
+    return "NO";
+}
+void main()
+{
+	int test;
+	scanf("%d",&test);
+	while(test)
+	{
+		char s1[100],s2[100];
+		scanf("%s",s1);
+		scanf("%s",s2);
+		char* result = twoStrings(s1, s2);
+		printf("%s",result);
+		test--;
+	}
+}
